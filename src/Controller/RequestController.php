0168f8a8bdf8e61586aed9f1567eed8876d65bf1
andrey <andrej_vmire@mail.ru>
2021-11-05 22:48:35 +0300

    + RequestController и представления для отображения запросов списком и по одному.

---
+++ src/Controller/RequestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/request")
+ */
+class RequestController extends AbstractController
+{
+    /**
+     * @Route("/", name="request list")
+     */
+    public function viewAction(): Response
+    {
+        $request_data = $this->getDoctrine()
+            ->getRepository(Request::class)
+            ->findAll();
+        return $this->render('request/view.html.twig',
+        [
+            "request_data" => $request_data,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="request view")
+     */
+    public function requestViewAction(int $id): Response
+    {
+        $request_data = $this->getDoctrine()
+            ->getRepository(Request::class)
+            ->find($id);
+        return $this->render('request/request.html.twig',[
+            "request_data" => $request_data,
+        ]);
+    }
+}
